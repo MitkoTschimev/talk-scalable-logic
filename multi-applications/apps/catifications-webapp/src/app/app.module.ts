cca81b2e30fc2a7e553762a17d534555da0f1e74
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2018-11-17 13:47:19 +0100

    add angular material

--- multi-applications/apps/catifications-webapp/src/app/app.module.ts
+++ multi-applications/apps/catifications-webapp/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { RouterModule } from '@angular/router';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     NxModule.forRoot(),
     RouterModule.forRoot([], { initialNavigation: 'enabled' })
   ],
