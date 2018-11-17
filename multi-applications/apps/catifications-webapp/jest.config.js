082899ba1b0fc60257d6133b72cf4aadbc61ee15
Mitko Tschimev <net.mitko.tschimev@mhp.com>
2018-11-17 13:27:03 +0100

    add webapp

---
+++ multi-applications/apps/catifications-webapp/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  name: 'catifications-webapp',
+  preset: '../../jest.config.js',
+  coverageDirectory: '../../coverage/apps/catifications-webapp/'
+};
---
+++ multi-applications/apps/catifications-webapp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'multi-applications-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'catifications-webapp';
+}
